38ec28eb51cd73a71c09c843178c37a247ac6c7c
Nathan <namo678@me.com>
2022-09-01 10:08:30 -0400

    commit tree 8e22a7747ad31676625a5b19a43842778f44cfb6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      begin
+        raise TriangleError
+        puts error.message
+      end
+    elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side3 + @side1 <= @side2
+      begin
+        raise TriangleError
+        puts error.message
+      end
+    elsif (@side1 == @side2 && @side2 == @side3)
+      :equilateral
+    elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
+      :isosceles
+    elsif @side1 != @side2 && @side2 != @side3
+      :scalene
+    end  
+  end
+  
+
+  class TriangleError < StandardError
+    def message
+      'Invalid Triangle.'
+    end
+  end
+
 end
+
+# t1 = Triangle.new(0, 0, 0).kind
+# puts t1
